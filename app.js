0f4e9f486c01020740526678f3533d5479abc9f1
Near-o7 <munoz.garriga@outlook.com>
2025-01-25 23:02:26 -0300

    Actauliza app.js y index.html para agregar nueva funcionalidad

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let friends = []
+
+function addFriend(){
+    friendName = document.querySelector('#amigo').value
+    console.log(friendName)
+    if (friendName === ""){
+        alert('Por favor, inserte un nombre.')
+    }else{
+        friends.push(friendName)
+        clearBox()
+        console.log(friends)
+    }
+}
+
+function clearBox(){
+    document.querySelector('#amigo').value = ''
+}
